d2cac94359be97cb598df0e1fc9cfadfba0c8a52
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-05-06 15:19:41 +0200

    feat: add Waypoint to manage animations

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import { ThemeProvider } from "styled-components";
+import { Waypoint } from "react-waypoint";
 import GlobalStyle, { dark, light } from "../theme/globalStyle";
 import AppHeader from "./AppHeader";
 import Input from "./Input";
 import PictureCard from "./PictureCard";
-import {today} from "../helpers"
+import { today } from "../helpers";
 
 function App() {
-
   const [isLightTheme, setLightTheme] = useState(false);
   const [date, setDate] = useState(today);
   const [pictureData, setPictureData] = useState("");
   const [animate, setAnimate] = useState(false);
-
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,13 +21,13 @@ function App() {
 
   const fetchData = async (date) => {
     try {
-      setAnimate(false);
+      //setAnimate(false);
       const response = await axios.get(
         `https://api.nasa.gov/planetary/apod?date=${date}&api_key=IrU9YCmzeRGcHbJULHNnNWTIhNitiAjxTegDI4XJ`
       );
       console.log(response.data);
       setPictureData(response.data);
-      setAnimate(true);
+      // setAnimate(true);
     } catch (error) {
       console.error(error);
     }
@@ -45,9 +43,13 @@ function App() {
         <GlobalStyle />
         <AppHeader setLightTheme={setLightTheme} isLightTheme={isLightTheme} />
 
-          <Input handleSubmit={handleSubmit} />
-          <PictureCard pictureData={pictureData} animate={animate} />
-
+        <Input handleSubmit={handleSubmit} />
+        <PictureCard pictureData={pictureData}  />
+        <Waypoint onEnter={() => setAnimate(true)}>
+          <div>
+            <PictureCard pictureData={pictureData} animate={animate} />
+          </div>
+        </Waypoint>
       </ThemeProvider>
     </Fragment>
   );
