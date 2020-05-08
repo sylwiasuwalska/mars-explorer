034e14ffd86ad7f1dc484bffbea533c7172d6e43
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-05-08 19:33:46 +0200

    feat: add numbers of favourite to icon desc

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import { Form, Jumbotron } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon} from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const JumbotronBackground = styled(Jumbotron)`
   background-image: url("https://apod.nasa.gov/apod/image/2004/STSCI-H-p2016a-m-2000x1374.jpg");
@@ -25,6 +25,21 @@ const AppTitle = styled.h1`
   }
 `;
 
+const FavouriteContainer = styled.span`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  font-weight: 500;
+  top: 30px;
+  left: 30px;
+  color: #000;
+  z-index: 10;
+
+  &.fa-layers-text {
+    font-size: 2rem;
+  }
+`;
+
 const ModeThemeChanger = styled.div`
   position: fixed;
   top: 30px;
@@ -37,7 +52,6 @@ const Paragraph = styled.p`
   margin-left: 10%;
   font-size: 1.5rem;
   color: #fff;
-
   @media (max-width: 768px) {
     margin: 0;
     font-size: 1rem;
@@ -45,8 +59,22 @@ const Paragraph = styled.p`
 `;
 
 function AppHeader(props) {
+
+
   return (
     <JumbotronBackground fluid>
+      <FavouriteContainer className="fa-layers fa-fw">
+        <FontAwesomeIcon
+          icon={faHeart}
+          color="white"
+          size="2x"
+          transform="left-3 down-1"
+        />
+        <span className="fa-layers-text" color="black">
+            {props.numberFavourite}
+        </span>
+      </FavouriteContainer>
+
       <ModeThemeChanger>
         <Form>
           <Form.Switch
@@ -55,15 +83,16 @@ function AppHeader(props) {
             id="custom-switch"
             label={
               props.isLightTheme ? (
-                <FontAwesomeIcon icon={faMoon}/>
+                <FontAwesomeIcon icon={faMoon} />
               ) : (
-                <FontAwesomeIcon icon={faSun}/>
+                <FontAwesomeIcon icon={faSun} />
               )
             }
             value="dark"
           />
         </Form>
       </ModeThemeChanger>
+
       <AppTitle>Astronomy Picture of the Day</AppTitle>
       <Paragraph>
         This is place where you can see pictures chosen by NASA as Astronomy
