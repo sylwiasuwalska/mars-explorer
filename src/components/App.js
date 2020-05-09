954254dfe242b1b54566701ca6a5bdaaf45edd7d
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-05-09 20:32:03 +0200

    refactor: remove unnecessary imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle, { dark, light } from "../theme/globalStyle";
